dbb460227121bee6a292fb38df66b1197643561f
Chryssa <chryssajones@gmail.com>
2017-09-23 12:18:55 -0700

    chryssa's changes

--- GailProject/assets/javascript.js
+++ GailProject/assets/javascript.js
@@ -30,14 +30,14 @@ function renderButtons() {
 $(document.body).on('click', '.petTypeButton', function() {
 	pets = $(this).attr("data-name");
 	$("#foundDiv").empty();
-	// for (i=0; i<10; i++) {
-		// console.log(i);
-		getPetFinder(pets);
-	// };
+	for (i=0; i<10; i++) {
+		console.log(i);
+		getPetFinder(i, pets);
+	};
 });
 
 
-function getPetFinder(type) {
+function getPetFinder(i, type) {
     var queryURL = "https://api.petfinder.com/pet.find?key=d39bdf5f87198eb7c88ec715df088049&format=json&animal=" + type + "&location=92129";
     $.ajax({
         type : 'GET',
@@ -46,27 +46,27 @@ function getPetFinder(type) {
         dataType: 'json'}).done(function(response) {
     		console.log(response);
     		// var length = response.petfinder.pets.pet.length;
-    		for(var i=0;i<6;i++) {
-        		var petDiv = $("<div class='gifs panel panel-info'>");
-        		var petBreed = response.petfinder.pets.pet[i].breeds.breed;
-                var petName = response.petfinder.pets.pet[i].name.$t;
-                var petSex = response.petfinder.pets.pet[i].sex.$t;
-                var petSize = response.petfinder.pets.pet[i].size.$t;
-                var petAge = response.petfinder.pets.pet[i].age.$t;
-                // var petStreet = response.petfinder.pets.pet[i].contact.address1
-                var petCity = response.petfinder.pets.pet[i].contact.city.$t;
-                var petState = response.petfinder.pets.pet[i].contact.state.$t;
-                var petZip = response.petfinder.pets.pet[i].contact.zip.$t;
-                var petID = response.petfinder.pets.pet[i].id.$t;
-                // var shelterId = response.petfinder.pets.pet[i].shelterID.$t;
-    			var breedLabel = $("<p>");
-    			 if(Array.isArray(petBreed)) {
-    				// console.log("Breed" + petBreed[0].$t);
-    				breedLabel.text("Breed: " + petBreed[0].$t + " (" + petAge + ")");
-    			} else if(typeof petBreed == 'object') {
-    				// console.log("Breed" + petBreed.$t);
-    				breedLabel.text("Breed: " + petBreed.$t + " (" + petAge + ")");
-		}
+    		// for(var i=0;i<10;i++) {
+    		var petDiv = $("<div class='gifs panel panel-info'>");
+    		var petBreed = response.petfinder.pets.pet[i].breeds.breed;
+            var petName = response.petfinder.pets.pet[i].name.$t;
+            var petSex = response.petfinder.pets.pet[i].sex.$t;
+            var petSize = response.petfinder.pets.pet[i].size.$t;
+            var petAge = response.petfinder.pets.pet[i].age.$t;
+            // var petStreet = response.petfinder.pets.pet[i].contact.address1
+            var petCity = response.petfinder.pets.pet[i].contact.city.$t;
+            var petState = response.petfinder.pets.pet[i].contact.state.$t;
+            var petZip = response.petfinder.pets.pet[i].contact.zip.$t;
+            var petID = response.petfinder.pets.pet[i].id.$t;
+            // var shelterId = response.petfinder.pets.pet[i].shelterID.$t;
+			var breedLabel = $("<p>");
+			 if(Array.isArray(petBreed)) {
+				// console.log("Breed" + petBreed[0].$t);
+				breedLabel.text("Breed: " + petBreed[0].$t + " (" + petAge + ")");
+			} else if(typeof petBreed == 'object') {
+				// console.log("Breed" + petBreed.$t);
+				breedLabel.text("Breed: " + petBreed.$t + " (" + petAge + ")");
+		// }
 
     		console.log(response.petfinder.pets.pet[i].media.photos.photo.length);
     		// console.log(response.petfinder.pets.pet[i].media.photos.photo[0].$id);
@@ -98,7 +98,7 @@ function getPetFinder(type) {
                 petDiv.append(petPic);
                 petDiv.attr("id", "pet" + i);
                 petDiv.addClass("container");
-                petDiv.css({"width":"250px", "display":"inline-grid", "margin":"15px", "padding":"30px", "text-align":"center"})
+                // petDiv.css({"width":"250px", "display":"inline-grid", "margin":"15px", "padding":"30px", "text-align":"center"})
     		 $("#resultsDiv").append(petDiv);
     		}
 	});
