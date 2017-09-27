84630f648f7acf4a591f77f4f2e7d4a010546a40
Lorenzo <lorenzo.bartolo@gmail.com>
2017-09-26 18:30:08 -0700

    Fixed Firebase and added back in javascript.js

--- assets/scripts/contact_me.js
+++ assets/scripts/contact_me.js
@@ -1,4 +1,3 @@
-
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyD7Tl5ZzIxV33LqQqvRyOvcDFKtc9Vselk",
@@ -46,7 +45,7 @@ $("#sendMessageButton").on('click', function () {
   $("#email").val("");
   $("#phone").val("");
 
- 
+
 });
 
 
@@ -58,61 +57,61 @@ $("#sendMessageButton").on('click', function () {
 
 //$(function() {
 
-  // $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
-  //   preventSubmit: true,
-  //   submitError: function($form, event, errors) {
-  //     // additional error messages or events
-  //   },
-  //   submitSuccess: function($form, event) {
-  //     event.preventDefault(); // prevent default submit behaviour
-  //     // get values from FORM
-  //     var name = $("input#name").val();
-  //     var email = $("input#email").val();
-  //     var phone = $("input#phone").val();
-  //     var message = $("textarea#message").val();
-  //     var firstName = name; // For Success/Failure Message
-  //     // Check for white space in name for Success/Fail message
-  //     if (firstName.indexOf(' ') >= 0) {
-  //       firstName = name.split(' ').slice(0, -1).join(' ');
-  //     }
-  //     $this = $("#sendMessageButton");
-  //     $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
-      // $.ajax({
-      //   url: "././mail/contact_me.php",
-      //   type: "POST",
-      //   data: {
-      //     name: name,
-      //     message: message
-      //   },
-      //   cache: false,
-      //   success: function() {
-      //     // Success message
-      //     $('#success').html("<div class='alert alert-success'>");
-      //     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-      //       .append("</button>");
-      //     $('#success > .alert-success')
-      //       .append("<strong>Your message has been sent. </strong>");
-      //     $('#success > .alert-success')
-      //       .append('</div>');
-      //     //clear all fields
-      //     $('#contactForm').trigger("reset");
-      //   },
-      //   error: function() {
-      //     // Fail message
-      //     $('#success').html("<div class='alert alert-danger'>");
-      //     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-      //       .append("</button>");
-      //     $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
-      //     $('#success > .alert-danger').append('</div>');
-      //     //clear all fields
-      //     $('#contactForm').trigger("reset");
-      //   },
-      //   complete: function() {
-      //     setTimeout(function() {
-      //       $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
-      //     }, 1000);
-      //   }
-      // });
+// $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+//   preventSubmit: true,
+//   submitError: function($form, event, errors) {
+//     // additional error messages or events
+//   },
+//   submitSuccess: function($form, event) {
+//     event.preventDefault(); // prevent default submit behaviour
+//     // get values from FORM
+//     var name = $("input#name").val();
+//     var email = $("input#email").val();
+//     var phone = $("input#phone").val();
+//     var message = $("textarea#message").val();
+//     var firstName = name; // For Success/Failure Message
+//     // Check for white space in name for Success/Fail message
+//     if (firstName.indexOf(' ') >= 0) {
+//       firstName = name.split(' ').slice(0, -1).join(' ');
+//     }
+//     $this = $("#sendMessageButton");
+//     $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+// $.ajax({
+//   url: "././mail/contact_me.php",
+//   type: "POST",
+//   data: {
+//     name: name,
+//     message: message
+//   },
+//   cache: false,
+//   success: function() {
+//     // Success message
+//     $('#success').html("<div class='alert alert-success'>");
+//     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+//       .append("</button>");
+//     $('#success > .alert-success')
+//       .append("<strong>Your message has been sent. </strong>");
+//     $('#success > .alert-success')
+//       .append('</div>');
+//     //clear all fields
+//     $('#contactForm').trigger("reset");
+//   },
+//   error: function() {
+//     // Fail message
+//     $('#success').html("<div class='alert alert-danger'>");
+//     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+//       .append("</button>");
+//     $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+//     $('#success > .alert-danger').append('</div>');
+//     //clear all fields
+//     $('#contactForm').trigger("reset");
+//   },
+//   complete: function() {
+//     setTimeout(function() {
+//       $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+//     }, 1000);
+//   }
+// });
 //     },
 //     filter: function() {
 //       return $(this).is(":visible");
@@ -129,4 +128,3 @@ $("#sendMessageButton").on('click', function () {
 // $('#name').focus(function() {
 //   $('#success').html('');
 // });
-
